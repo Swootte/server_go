edbec40c1c51376f32ef1f1f340d2247d9d0c298
prince ONDONDA <princeondonda@MacBook-Pro.local>
2022-09-20 07:42:50 +0200

    debug online

--- user/user.go
+++ user/user.go
@@ -153,6 +153,7 @@ func CreateUser(ctx context.Context, user *model.UserInput, uid string, ip strin
 func GetUserByFirebaseId(uid string) (*model.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	fmt.Println(os.Getenv("DATABASE"), "this is thee database")
 	_collections := database.MongoClient.Database(os.Getenv("DATABASE")).Collection("users")
 	var singleUser *model.User
 	err := _collections.FindOne(ctx, bson.D{{Key: "firebaseUID", Value: uid}}).Decode(&singleUser)
@@ -191,6 +192,7 @@ func MigrateAllUsersWallet() {
 }
 
 func UserExist(uid string) (bool, error) {
+	fmt.Println("i'm goin through", uid)
 	res, err := GetUserByFirebaseId(uid)
 	if !*res.Deleted {
 		return true, err
